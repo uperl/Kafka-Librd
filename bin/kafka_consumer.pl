ccd55234f33b7ccf6124f2377cc4e800a4640359
Pavel Shaydo <zwon@trinitum.org>
2017-02-04 22:54:19 +0100

    improve example scripts

--- bin/kafka_consumer.pl
+++ bin/kafka_consumer.pl
@@ -5,22 +5,92 @@ use warnings;
 
 use Kafka::Librd;
 use Getopt::Long;
+use Pod::Usage;
 
-GetOptions(
+my $res = GetOptions(
     "group-id=s" => \my $group_id,
     "topic=s"    => \my @topics,
     "brokers=s"  => \my $brokers,
+    "ca-cert=s"  => \my $cacert,
+    "username=s" => \my $sasluser,
+    "password=s" => \my $saslpass,
     "debug"      => \my $debug,
+    "help"       => \my $help,
 );
+
+pod2usage(-verbose => 2, -noperldoc => 1) if $help or not $res;
+
+=head1 SYNOPSIS
+
+     kafka_consumer.pl [options] [file...]
+
+=head1 DESCRIPTION
+
+The script connects to Kafka, subscribes to specified topic and outputs incomming messages.
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<group-id>
+
+consumer group id to use, by default it is "test-consumer"
+
+=item B<topic>
+
+topic to which to subscribe
+
+=item B<brokers>
+
+comma separated list of brokers. For example: C<broker1.mydomain:9092,broker2.mydomain:9092>.
+
+=item B<ca-cert>
+
+path to CA certificate. If not specified then plaintext protocol is used to connect to broker.
+
+=item B<username>
+
+=item B<password>
+
+username and password for authentication if required
+
+=item B<debug>
+
+print additional debugging information
+
+=back
+
+=cut
 
 $group_id //= "test-consumer";
 $brokers  //= "localhost:9092";
+
+my $sproto = $cacert ? "ssl" : "plaintext";
+$sproto = "sasl_$sproto" if $sasluser;
 
 my $kafka = Kafka::Librd->new(
     Kafka::Librd::RD_KAFKA_CONSUMER,
     {
         'group.id' => $group_id,
+        (
+            $cacert
+            ? (
+                'security.protocol' => $sproto,
+                'ssl.ca.location'   => $cacert,
+              )
+            : ()
+        ),
+        (
+            $sasluser
+            ? (
+                'sasl.mechanisms' => 'PLAIN',
+                'sasl.username'   => $sasluser,
+                'sasl.password'   => $saslpass,
+              )
+            : ()
+        ),
         ( $debug ? ( debug => 'cgrp,topic,fetch' ) : () ),
+        'api.version.request' => 'true',
     },
 );
 
