d352edfb57aafa6395d4b889fee60fd76c49cde5
Graham Ollis <plicease@cpan.org>
2021-04-19 03:55:59 -0600

    switch to gh actions for CI

--- lib/Kafka/Librd.pm
+++ lib/Kafka/Librd.pm
@@ -315,7 +315,7 @@ L<https://github.com/edenhill/librdkafka>
 =head1 BUGS
 
 Please report any bugs or feature requests via GitHub bug tracker at
-L<http://github.com/plicease/Kafka-Librd/issues>.
+L<http://github.com/uperl/Kafka-Librd/issues>.
 
 =head1 AUTHOR
 
